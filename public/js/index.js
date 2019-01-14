535cb124fecbd8510f58cbb7147c47731bc148e4
the-machine-learner <ritvikush@gmail.com>
2019-01-14 16:43:38 +0530

    using createMessage to give all users a message

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,7 @@
 	var socket = io();
 	socket.on('connect',function(){
-		console.log('connected to server');
-		
+		console.log('new user connected');
+
 		socket.emit('createMessage',{
 			to: 'user@gc',
 			text: 'reply'
@@ -13,3 +13,6 @@
 	socket.on('newMessage',function(Message){
 		console.log('new Message',Message);
 	});
+
+// socket.emit VS io.emmit - io emmits to every single listner 
+//while socket emmits to a single listner 
